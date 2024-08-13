9ff5c555cbe15bf43222a7dc6331ee31c9718ef8
Aditya <adityamhetredata@gmail.com>
2024-08-13 09:34:02 +0530

    first Commit

---
+++ check.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+# import re
+
+if __name__ == '__main__':
+    # Create a Spark session
+    spark = SparkSession.builder \
+        .appName("OracleToSpark") \
+        .config("spark.driver.extraClassPath", r"C:\Jar_Files\ojdbc8.jar") \
+        .getOrCreate()
+    print(spark)
